33219f1c877e0a0923d54983bb299abfa4dc935d
Tyler Boyd <ty@tyboyd.dev>
2023-10-21 13:26:17 -0500

    added update, read one, and delete routes

--- controllers/books.go
+++ controllers/books.go
@@ -11,11 +11,27 @@ type CreateBookInput struct {
 	Author string `json:"author" binding:"required"`
 }
 
+type UpdateBookInput struct {
+	Title  string `json:"title"`
+	Author string `json:"author"`
+}
+
 func FindBooks(c *gin.Context) {
 	var books []models.Book
 	models.DB.Find(&books)
 
 	c.JSON(http.StatusOK, gin.H{"data": books})
+}
+
+func FindBook(c *gin.Context) {
+	var book models.Book
+
+	if err := models.DB.Where("id = ?", c.Param("id")).First(&book).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": book})
 }
 
 func CreateBook(c *gin.Context) {
@@ -31,4 +47,39 @@ func CreateBook(c *gin.Context) {
 	models.DB.Create(&book)
 
 	c.JSON(http.StatusOK, gin.H{"data": book})
+}
+
+func UpdateBook(c *gin.Context) {
+	// find book
+	var book models.Book
+
+	if err := models.DB.Where("id = ?", c.Param("id")).First(&book).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found"})
+		return
+	}
+
+	// validate input
+	var input UpdateBookInput
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// make update
+	models.DB.Model(&book).Updates(input)
+	c.JSON(http.StatusOK, gin.H{"data": book})
+}
+
+func DeleteBook(c *gin.Context) {
+	// find book
+	var book models.Book
+
+	if err := models.DB.Where("id = ?", c.Param("id")).First(&book).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found"})
+		return
+	}
+
+	// delete book
+	models.DB.Delete(&book)
+	c.JSON(http.StatusOK, gin.H{"data": true})
 }
--- main.go
+++ main.go
@@ -13,11 +13,16 @@ func main() {
 
 	models.ConnectDatabase()
 
+	// root route
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"data": "good call"})
 	})
+	// book routes
 	r.GET("/books", controllers.FindBooks)
+	r.GET("/books/:id", controllers.FindBook)
 	r.POST("/books", controllers.CreateBook)
+	r.PUT("/books/:id", controllers.UpdateBook)
+	r.DELETE("/books/:id", controllers.DeleteBook)
 
 	err := r.Run()
 	if err != nil {
